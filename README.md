ef4a01c488e0953750461a7cf08c68cd2d17e8cb
丘天惠 <43975076+Autumnhui@users.noreply.github.com>
2020-02-19 22:19:51 +0800

    上传相关代码以及示例数据、图片

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # WordCloud-DouBan_Topic
+
  爬取豆瓣话题制作词云。
+
+## 🏔️示例
+
+> 豆瓣话题——你觉得最好的理财方式是什么？
+https://www.douban.com/gallery/topic/20737/?from=gallery_hot_post
+
+![1](输出结果.jpg)
--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import jieba
 
+
+# 页面刷新时，变化的是接口中start的参数，为从0-200的间隔20的数字。⬇️获取所有请求数据。
 for i in range(0,200,20):
-    # 通过浏览器检查，得到数据的URL来源链接
+    # 开发者工具-网络中items..的文件为数据，查看Headers获取网页请求的Requests Url
     url = 'https://m.douban.com/rexxar/api/v2/gallery/topic/20737/items?' \
           'sort=hot&start=0&count=20&status_full_text=1&guest_only=0&ck=null'.format(i)
 
-    # 破解防爬虫，带上请求头
-    # 这两个不能省略
+    # 豆瓣反爬虫机制，需带上请求头。
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0".3809.100 Safari/537.36',
                'Referer': 'https://www.douban.com/gallery/topic/20737/?from=gallery_hot_post'}
 
@@ -19,27 +20,26 @@ for i in range(0,200,20):
     reponse = requests.get(url, headers=headers)
     html = reponse.json()
 
-    # 解析数据，获得短评
-    # 保存到本地
+    # 解析数据，并保存到本地
     for j in range(19):
         abs = html['items'][j]['abstract']
-        with open("want_after.txt", "a", encoding='utf-8') as f:
+        with open("data.txt", "a", encoding='utf-8') as f:
             f.write(abs)
             print(abs)
 
-# 获得wordcloud 需要的 文本格式
-with open("want_after.txt", "r", encoding='utf-8') as f:
+# 获得wordcloud 需要的文本格式
+with open("data.txt", "r", encoding='utf-8') as f:
      text = ' '.join(jieba.cut(f.read(),cut_all=False))
      # print(text)
 
-backgroud_Image = plt.imread('背景图.png')  # 背景图
+backgroud_Image = plt.imread('背景图.png')  # 背景图，生成图片的大小为背景图的大小
 
 # 词云的一些参数设置
 wc = WordCloud(
       background_color='white',
       mask=backgroud_Image,
       font_path='pixel.ttf',
-      max_words=700,
+      max_words=400,
       max_font_size=200,
       min_font_size=8,
       random_state=50,
@@ -64,4 +64,3 @@ wc.to_file('输出结果.jpg')
 # df = pd.DataFrame(sort_after)
 # # 保证不乱码
 # df.to_csv('sort_after.csv', encoding='utf_8_sig')
-#
